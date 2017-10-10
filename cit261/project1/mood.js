9ff0971d6c928ac8db026a412d26e4b297b406a8
blainard91 <Blaine@Blaines-New-MacBook-Pro.local>
2017-10-10 16:39:28 -0600

    Touch stuff

--- cit261/project1/mood.js
+++ cit261/project1/mood.js
@@ -1,1 +1,31 @@
+window.onload = function() {
+    var grey = document.getElementById("grey");
+    var container = document.getElementById("container");
+    var scanner = document.getElementById("scanner");
+    console.log(grey.style.opacity);
+
+    container.addEventListener('touchstart', function(){
+        if (grey.style.opacity > "0"){
+            scanner.style.opacity = "0";
+        } else {
+            scanner.style.opacity = "1";
+            }
+    })
+
+    container.addEventListener('touchend', function(){
+            grey.style.opacity = "0.7";
+            scanner.style.opacity = "0";
+    })
+
+    grey.addEventListener('touchstart', function(){
+        if (grey.style.opacity > "0"){
+            grey.style.opacity = "0";
+        }
+    })
+}
+
+
+
+
+
 
