776f72baa999a034fef5e8224859d548cd9e37e9
blainard91 <Blaine@Blaines-New-MacBook-Pro.local>
2017-10-11 12:30:34 -0600

    z-index stuff

--- cit261/project1/mood.js
+++ cit261/project1/mood.js
@@ -3,6 +3,7 @@ window.onload = function() {
     var container = document.getElementById("container");
     var scanner = document.getElementById("scanner");
     var mood = document.getElementById("mood");
+    var body = document.getElementById("body");
 
     container.addEventListener('touchstart', function(){
         if (grey.style.opacity > "0"){
@@ -16,7 +17,7 @@ window.onload = function() {
             grey.style.opacity = "0.7";
             scanner.style.opacity = "0";
             mood.style.opacity = "1"
-            mood.style.zIndex = "99999";
+            mood.style.zIndex = "9999";
     })
 
     grey.addEventListener('touchstart', function(){
@@ -24,6 +25,13 @@ window.onload = function() {
             grey.style.opacity = "0";
             mood.style.opacity = "0"
             mood.style.zIndex = "9";
+        }
+    })
+    mood.addEventListener('touchstart', function(){
+        if (grey.style.opacity > "0"){
+             grey.style.opacity = "0";
+             mood.style.opacity = "0"
+             mood.style.zIndex = "9";
         }
     })
 }
