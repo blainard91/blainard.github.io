18a4552d0252efc6613e75bf66f6dd10f005d1bc
blainard91 <Blaine@Blaines-New-MacBook-Pro.local>
2017-10-11 11:03:36 -0600

    Mood Box

--- cit261/project1/mood.js
+++ cit261/project1/mood.js
@@ -2,7 +2,7 @@ window.onload = function() {
     var grey = document.getElementById("grey");
     var container = document.getElementById("container");
     var scanner = document.getElementById("scanner");
-    console.log(grey.style.opacity);
+    var mood = document.getElementById("mood");
 
     container.addEventListener('touchstart', function(){
         if (grey.style.opacity > "0"){
@@ -15,11 +15,15 @@ window.onload = function() {
     container.addEventListener('touchend', function(){
             grey.style.opacity = "0.7";
             scanner.style.opacity = "0";
+            mood.style.opacity = "1"
+            mood.style.zIndex = "99999";
     })
 
     grey.addEventListener('touchstart', function(){
         if (grey.style.opacity > "0"){
             grey.style.opacity = "0";
+            mood.style.opacity = "0"
+            mood.style.zIndex = "9";
         }
     })
 }
